6ca6cac4b13a5ab6484e404431045e6549dcec1b
madzik890 <madzik890@hotmail.com>
2024-11-19 17:30:46 +0100

    Fix premissions on run scripts

--- debian11_amd64/run_debian11_amd64.sh
+++ debian11_amd64/run_debian11_amd64.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker container stop debian11amd64
 docker container remove debian11amd64
-docker run -v /home/msucharski:/home/msucharski --name debian11amd64 --network host -it debian11_amd64
+docker run -v /home/msucharski:/home/msucharski:z --name debian11amd64 --network host -it debian11_amd64
--- debian11_arm64/run_debian11_arm64.sh
+++ debian11_arm64/run_debian11_arm64.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker container stop debian11arm64
 docker container remove debian11arm64
-docker run -v /home/msucharski:/home/msucharski --name debian11arm64 --network host -it debian11_arm64
+docker run -v /home/msucharski:/home/msucharski:z --name debian11arm64 --network host -it debian11_arm64
