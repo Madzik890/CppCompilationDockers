54a1ad44731ec2e60dd47f95c77f426b01e09ffa
madzik890 <madzik890@hotmail.com>
2024-11-19 22:48:38 +0100

    Add wiringpi for debian11_arm64

--- debian11_arm64/run_debian11_arm64.sh
+++ debian11_arm64/run_debian11_arm64.sh
@@ -2,4 +2,4 @@
 
 docker container stop debian11arm64
 docker container remove debian11arm64
-docker run -v /home/msucharski:/home/msucharski:z --name debian11arm64 --network host -it debian11_arm64
+docker run -v /home/msucharski:/home/msucharski:z -v /home/debian11arm64:/root/.conan2/p:z --name debian11arm64 --network host -it debian11_arm64
