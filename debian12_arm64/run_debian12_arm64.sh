b659a369e6043c8afeaf2003855407f7db6f9757
madzik890 <madzik890@hotmail.com>
2025-01-05 15:58:33 +0100

    Add library needed to compile QT

--- debian12_arm64/run_debian12_arm64.sh
+++ debian12_arm64/run_debian12_arm64.sh
@@ -2,4 +2,4 @@
 
 docker container stop debian12arm64
 docker container remove debian12arm64
-docker run -v /home/msucharski:/home/msucharski:z -v /home/debian12arm64:/root/.conan2/p:z --name debian12arm64 --network host -it debian12_arm64
+docker run --restart unless-stopped -v /home/msucharski:/home/msucharski:z -v /home/debian12arm64:/root/.conan2/p:z --name debian12arm64 --network host -it debian12_arm64
